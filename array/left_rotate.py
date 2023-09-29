472ba2a3279854980823383af73da769267c466a
unknown <kaniket76680@gmail.com>
2023-09-30 01:21:29 +0530

    left rotate by d approach 2

--- array/left_rotate.py
+++ array/left_rotate.py
@@ -11,3 +11,22 @@ class LeftRotate:
         for j in range(d):
             result.append(arr[j])
         return result
+    
+    ### Approach 2
+    def left_rotate(self,arr,d):
+        n=len(arr)
+        if n==0:
+            return None
+        self.reverse(arr,0,d-1)
+        self.reverse(arr,d,n-1)
+        self.reverse(arr,0,n-1)
+        return arr
+
+    def reverse(self,arr,start,end):
+        while start<end:
+            # arr[start],arr[end] = arr[end],arr[start]
+            arr[start] = arr[start]+arr[end]
+            arr[end] = arr[start]-arr[end]
+            arr[start]=arr[start]-arr[end]
+            start+=1
+            end-=1
