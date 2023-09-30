9d871c5cd91ba756de5f5f4f35d3f9a405a2b7c3
unknown <kaniket76680@gmail.com>
2023-10-01 00:28:34 +0530

    delete operation in heapify

--- Heap_priorityQueue.py/basic_operation.py
+++ Heap_priorityQueue.py/basic_operation.py
@@ -24,13 +24,20 @@ class BasicOperation:
         arr[n2]=temp
         
         
-        
-        
-        
-        
-        
+    ##########  DELETE OPEREATION ################
+    def delete_in_heapify(self,arr,index):
+        if len(arr)==0 or index>len(arr):
+            return 
+        n=len(arr)
+        arr[index] = arr[n-1]
+        del arr[n-1]
+        n-=1
+        for i in range(int((n-1)/2),-1,-1):
+            self.topdown_heapify(arr,n,i)
+        return arr
         
         
 arr=[20, 11, 10, 2, 3, 1, 4]
 BasicOperation = BasicOperation()
 print(BasicOperation.insert_heapify(arr,5))
+print(BasicOperation.delete_in_heapify(arr,1))
