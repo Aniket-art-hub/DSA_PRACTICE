d73e666580b41dcb86fe9ce25aca042a28d8922f
unknown <kaniket76680@gmail.com>
2023-10-01 00:34:24 +0530

    peek operation in heapify

--- Heap_priorityQueue.py/basic_operation.py
+++ Heap_priorityQueue.py/basic_operation.py
@@ -24,7 +24,7 @@ class BasicOperation:
         arr[n2]=temp
         
         
-    ##########  DELETE OPEREATION ################
+    ##########  DELETE OPEREATION comlexity is o(logn) ################
     def delete_in_heapify(self,arr,index):
         if len(arr)==0 or index>len(arr):
             return 
@@ -35,9 +35,16 @@ class BasicOperation:
         for i in range(int((n-1)/2),-1,-1):
             self.topdown_heapify(arr,n,i)
         return arr
+    
+    def get_peek(self,arr):  ## complexity is O(1)
+        n=len(arr)
+        if n==0:
+            return 0 
+        return arr[0]
         
         
 arr=[20, 11, 10, 2, 3, 1, 4]
 BasicOperation = BasicOperation()
 print(BasicOperation.insert_heapify(arr,5))
 print(BasicOperation.delete_in_heapify(arr,1))
+print(BasicOperation.get_peek(arr))
