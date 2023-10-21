d897ef789a5c1c1ca8ea42a53d7b65d78c908274
unknown <kaniket76680@gmail.com>
2023-10-22 00:44:44 +0530

    fibonacci series by tabulation method

--- dynamic_programming/fibonacci_series.py
+++ dynamic_programming/fibonacci_series.py
@@ -19,4 +19,17 @@ class Solution:
         if array[n] == False:
             array[n] =self.fibmemorisation(n-1,array) + self.fibmemorisation(n-2,array)
         return array[n]
+    
+    ## by using tabulation technique
+    
+    def fibTabulation(self,n,array):
+        if n==0:
+            return n
+        if len(array)>1:
+            array[0]=0
+            array[1]=1
+        for i in range(2,n+1):
+            array[i] = array[i-2]+array[i-1]
+        return array[n]
+    
         
